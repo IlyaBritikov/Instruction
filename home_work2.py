cdf5fbbc864781a268e49be9e661cd6c13359f2d
Ilya Britikov <razdv4@gmail.com>
2024-03-21 12:47:28 +0300

    new_

--- home_work2.py
+++ home_work2.py
@@ -1,44 +1,26 @@
 def decode_instruction(instruction: str) -> str:
-    # Функция для декодирования сжатой инструкции
-
-    stack: List[str] = []
+    stack = []
+    current_str = ''
+    current_num = 0
 
     for char in instruction:
-        if char == ']':
-            decoded_str = ''
-            for sign in stack[::-1]:
-                if sign == '[':
-                    stack.pop()  # Удаляем '['
-                    break
-                else:
-                    decoded_str = stack.pop() + decoded_str
-
-            num_str = ''
-            for sign in stack[::-1]:
-                if sign.isdigit():
-                    num_str = stack.pop() + num_str
-                else:
-                    break
-            num = int(num_str)
-
-            stack.append(decoded_str * num)
+        if char.isdigit():
+            current_num = current_num * 10 + int(char)
+        elif char == '[':
+            stack.append((current_str, current_num))
+            current_str = ''
+            current_num = 0
+        elif char == ']':
+            prev_str, prev_num = stack.pop()
+            current_str = prev_str + current_str * prev_num
         else:
-            stack.append(char)
+            current_str += char
 
-    decode_instruction = ''
-    for item in stack:
-        if isinstance(item, tuple):
-            decode_instruction += item[0] * item[1]
-        else:
-            decode_instruction += item
-    return decode_instruction            
+    return current_str
 
-def main():
-    compressed_instruction = input().strip()
+# Считываем сжатую команду из ввода
+compressed_instruction = input().strip()
 
-    decoded_instruction = decode_instruction(compressed_instruction)
-    print(decoded_instruction)
-
-
-if __name__ == '__main__':
-    main()
+# Расшифровываем команду и выводим результат
+decoded_instruction = decode_instruction(compressed_instruction)
+print(decoded_instruction)
